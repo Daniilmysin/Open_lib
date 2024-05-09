0c243c1f6718036f59a6837bb7b6220cb6b5c33a
daniilmysin <mysya.d@ya.ru>
2024-05-10 00:32:45 +0200

    1

--- DBScripts.py
+++ DBScripts.py
@@ -4,17 +4,14 @@ from sqlalchemy.dialects.postgresql import TSVECTOR
 import asyncio
 import logging
 from sqlalchemy.orm import DeclarativeBase, relationship
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncAttrs
 
 logging.basicConfig(level=logging.INFO, filename="py_bot.log", format="%(asctime)s %(levelname)s %(message)s")
 user= input("введите логин: ")
 passwd= input("введите пароль: ")
-host= input("введите хост")
+host= input("введите хост: ")
 engine = create_async_engine(f"postgresql+asyncpg://{user}:{passwd}@{host}/postgres")
-session = async_sessionmaker(engine, expire_on_commit=False)
+session = AsyncSession(engine)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -30,15 +27,15 @@ class book(Base):
     Date_birth= Column(Date, nullable=False)
     user_id= Column(Integer, ForeignKey('users.id'))
     check= Column(Boolean)
-    adres_epub= Column(String(200),nullable=False)
-    adres_pdf = Column(String(200))
+    adres= Column(String(200),nullable=False)
     
 class author(Base):
     __tablename__='author'
     id= Column(Integer, primary_key=True)
     name=Column(String(100),nullable=False)
     last_name=Column(String(100),nullable=False)
-    patronymic = Column(String(100)) #отчество
+    patronymic= Column(String(100)) #отчество
+    photo= Column(String(100))
     books = relationship("book")
 
 class users(Base):
@@ -46,11 +43,15 @@ class users(Base):
     id= Column(Integer, primary_key=True)
     like_books= Column(Integer)
 
+async def searcher():
 
-async def main():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    pass
 
+async def book_ret(id,type=".epub"): #возвращает адрес книги по айди
+    await session.connection()
+    adres=await session.query(book).get(id)
+    await session.commit()
+    await session.close()
+    adres="books"+adres+type
+    return adres
 
-if __name__ == '__main__':
-    asyncio.run(main())
